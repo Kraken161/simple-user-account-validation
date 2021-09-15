c6d7a4124c5709a75fae3c35fe750758c11a3124
Kraken161 <66274761+Kraken161@users.noreply.github.com>
2021-09-15 22:24:36 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose')
+const CryptoJS = require('crypto-js')
+
+const Schema = mongoose.Schema
+const secretKey = 'dsasdasdrrth%^$$%^TERGDFDFG' // must be secret in .env file
+
+mongoose.connect('mongodb://localhost:27017/db', {
+	// your own mongodb url
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+
+const validateEmail = (email) => {
+	const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+	return re.test(email)
+}
+
+const encryptPassword = (password, key) => {
+	const encryptedPassword = CryptoJS.AES.encrypt(password, key).toString()
+
+	return encryptedPassword
+}
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: [true, 'The name field is required'],
+		minLength: [3, ' The minimum number of characters is 3'],
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: [true, 'The name field is required'],
+		trim: true,
+		lowercase: true,
+		unique: true,
+		validate: [(value) => validateEmail(value), 'Please enter a valid email!'],
+	},
+	password: {
+		type: String,
+		required: [true, 'The password field is required'],
+	},
+	isAdmin: {
+		type: Boolean,
+		default: false,
+	},
+})
+
+const User = mongoose.model('User', userSchema)
+
+async function main() {
+	const newUser = new User({
+		name: 'sample',
+		email: 'sample@gmail.com',
+		password: encryptPassword('SamplePassword', secretKey),
+	})
+
+	try {
+		await newUser.save((err, data) => {
+			if (err) {
+				if (err.code === 11000) {
+					return console.log('This email already exists!')
+				}
+			}
+		})
+	} catch (e) {
+		console.log('Something went wrong...')
+		for (const key in e.errors) {
+			console.log(e.errors[key].message)
+		}
+	}
+}
+
+//main()
+
+async function getUsers() {
+	const users = await User.find({})
+
+	console.log(users)
+}
+
+// getUsers()
+
+async function getUser() {
+	const user = await User.find({ email: 'Sample Email' })
+
+	console.log(user)
+}
+
+//getUser()
